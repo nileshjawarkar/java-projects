0cc1597a3f0c882839c5a33f88bb23a04b2dded1
nileshjawarkar <jawarkar.nilesh@gmail.com>
2024-02-16 22:23:50 +0530

    Persistance Part 01

--- carman/src/main/java/com/nnj/learn/javaee8/entity/Car.java
+++ carman/src/main/java/com/nnj/learn/javaee8/entity/Car.java
@@ -1,8 +1,28 @@
 package com.nnj.learn.javaee8.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "cars")
+@NamedQuery(name = Car.FIND_ALL_CARS, query = "select c from Car c")
+@NamedQuery(name = Car.FIND_A_CAR, query = "select c from Car c where c.id = :carId")
 public class Car {
+	
+	public static final String FIND_ALL_CARS = "Car.FindAll";
+	public static final String FIND_A_CAR = "Car.FindACar";
+	
+	@Id
 	private String id;
+	
+	@Enumerated(EnumType.STRING)
 	private EngineType engineType;
+	
+	@Enumerated(EnumType.STRING)
 	private Color color;
 
 	public Color getColor() {
