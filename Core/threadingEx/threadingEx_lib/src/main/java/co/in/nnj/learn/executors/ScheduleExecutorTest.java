e8874a38c8b9b3ca7ef15181d33561fe684f9a03
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-02 22:22:21 +0530

    changes to job and time periods

--- Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/executors/ScheduleExecutorTest.java
+++ Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/executors/ScheduleExecutorTest.java
@@ -22,7 +22,7 @@ public class ScheduleExecutorTest {
             final long now = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
             System.out.println(Thread.currentThread().getName() + "-" + now + " => Id = " + id + ", scheduledAt = " + timePeriod);
             try {
-                TimeUnit.SECONDS.sleep(1);
+                TimeUnit.SECONDS.sleep(10);
             } catch (final InterruptedException e) {
                 e.printStackTrace();
             }
@@ -31,16 +31,15 @@ public class ScheduleExecutorTest {
 
    public static void main(final String[] args) {
         final ScheduledExecutorService service = Executors.newScheduledThreadPool(1) ;
-        int timePeriod = 2;
+        final int tpList[] = {10, 30, 50};
         for (int i = 0; i < 3; i++) {
-            final Task t = new Task(i + 1, timePeriod);
-            service.scheduleWithFixedDelay(t, INITIAL_DELAY, timePeriod, TimeUnit.SECONDS);
-            //-- service.scheduleAtFixedRate(t, INITIAL_DELAY, timePeriod, TimeUnit.SECONDS);
-            timePeriod += 2;
+            final Task t = new Task(i + 1, tpList[i]);
+            //-- service.scheduleWithFixedDelay(t, INITIAL_DELAY, tpList[i], TimeUnit.SECONDS);
+            service.scheduleAtFixedRate(t, INITIAL_DELAY, tpList[i], TimeUnit.SECONDS);
         }
 
         try {
-            TimeUnit.SECONDS.sleep(13);
+            TimeUnit.MINUTES.sleep(1);
         } catch (final InterruptedException e) {
             e.printStackTrace();
         }
