eebf1d212e5bcab85f82ee6792dd751d80e6522c
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-01-15 07:18:50 +0530

    Added some more examples

--- Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/streams/Ex5_MatchAndFind.java
+++ Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/streams/Ex5_MatchAndFind.java
@@ -2,6 +2,7 @@ package co.in.nnj.learn.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import co.in.nnj.learn.streams.model.Book;
 import co.in.nnj.learn.streams.model.Book.Type;
@@ -27,8 +28,35 @@ public class Ex5_MatchAndFind {
                 .withTitle("Book5").withAuthor("Author2")
                 .withPages(501).withPrice(500).get());
 
-        final boolean allMatch1 = books.stream().map(b -> b.getPages()).allMatch(p -> p >= 500);
-        final boolean allMatch2 = books.stream().allMatch(b -> b.getPages() >= 100);
-        System.out.println(allMatch1 + ", " + allMatch2);
+        boolean allMatch = books.stream().allMatch(b -> b.getPages() >= 100);
+        System.out.println("Is all elems gretorthan 100 : " + allMatch);
+
+        allMatch = books.stream().allMatch(b -> b.getPages() >= 300);
+        System.out.println("Is all elems gretorthan 300 : " + allMatch);
+
+        boolean noneMatch = books.stream().noneMatch(b -> b.getPages() > 500);
+        System.out.println("Is none of them gretorthan 500 : " + noneMatch);
+
+        noneMatch = books.stream().noneMatch(b -> b.getPages() > 700);
+        System.out.println("Is none of them gretorthan 700 : " + noneMatch);
+
+        final boolean anyOne = books.stream().anyMatch(b -> b.getPages() > 500);
+        System.out.println("Is one of them gretorthan 500 : " + anyOne);
+
+        final Optional<Book> any = books.stream().filter(b -> b.getPages() > 400).findAny();
+        final long count = books.stream().filter(b -> b.getPages() > 400).count();
+        System.out.println("any of " + count + " gretorthan 400 =" + any.get());
+
+        final Optional<Book> first = books.stream()
+                .filter(b -> b.getPages() > 400)
+                .sorted((a, b) -> {
+                    if (a.getPages() > b.getPages()) {
+                        return -1;
+                    } else if (a.getPages() < b.getPages()) {
+                        return 1;
+                    }
+                    return 0;
+                }).findFirst();
+        System.out.println("First of " + count + " gretorthan 400 =" + first.get());
     }
 }
