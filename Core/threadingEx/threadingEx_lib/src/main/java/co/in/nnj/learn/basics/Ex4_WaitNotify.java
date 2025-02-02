75dd7e2cd4ca37cae35dc554c1273b4863a0b245
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-02-02 18:11:29 +0530

    Added comments

--- Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/basics/Ex4_WaitNotify.java
+++ Core/threadingEx/threadingEx_lib/src/main/java/co/in/nnj/learn/basics/Ex4_WaitNotify.java
@@ -4,6 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+//-- Must follow this struture for wait and notity
+//-- Producer:
+//-- synchronized(lock) {
+//--    .. Perform the task and notify
+//--    lock.notify();
+//--    .. Wait for other thread to notify
+//--    lock.wait();
+//-- }
+//-- Consumer:
+//-- synchronized(lock) {
+//--    .. Wait for other thread to notify
+//--    lock.wait();
+//--    .. Perform the task and notify other thread then wait
+//--    lock.notify();
+//-- }
+
 public class Ex4_WaitNotify {
 
     public static class Worker {
@@ -11,17 +27,20 @@ public class Ex4_WaitNotify {
         private final Object lock = new Object();
 
         private int counter = 0;
-        private final int MAX_LEN = 4;
+        private final int MAX_JOB_SIZE = 4;
 
         public void producer() throws InterruptedException {
+            // -- Producer add 4 (MAX_JOB_SIZE) messages to list,
+            // -- then notify the other thread, so that it can acquire the lock
+            // -- and proceed. And it self call the wait and wait other thread to notify it.
             synchronized (lock) {
                 while (counter < 40) {
-                    if (jobList.size() < MAX_LEN) {
+                    if (jobList.size() < MAX_JOB_SIZE) {
                         final String str = "JobNumber-" + counter;
                         jobList.add(str);
                         counter++;
                         System.out.println("Added - " + str);
-                        if (jobList.size() == MAX_LEN) {
+                        if (jobList.size() == MAX_JOB_SIZE) {
                             lock.notify();
                         }
                     } else {
@@ -33,9 +52,14 @@ public class Ex4_WaitNotify {
         }
 
         public void consumer() throws InterruptedException {
+            // -- Consumer check the jobList size, if it is zero,
+            // -- it will wait for the other thread to add jobs and notify.
+            // -- When other thread notify and it will acquire the lock and 
+            // -- read the jobs. When jobList become empty, it notify the other Thread
+            // -- to continue and goes to wait state.
             synchronized (lock) {
                 while (counter < 40 || jobList.size() > 0) {
-                    if ( jobList.size() == 0 ) {
+                    if (jobList.size() == 0) {
                         System.out.println("Waiting producer to produce the jobs");
                         lock.wait();
                     } else {
@@ -61,7 +85,6 @@ public class Ex4_WaitNotify {
                     e.printStackTrace();
                 }
             }
-
         });
 
         final Thread t2 = new Thread(new Runnable() {
@@ -74,7 +97,6 @@ public class Ex4_WaitNotify {
                     e.printStackTrace();
                 }
             }
-
         });
 
         t1.start();
