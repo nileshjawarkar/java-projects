669abf5c43f99f6a9e4c21dbd14ff910085afe45
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-02-16 09:49:31 +0530

    Example refactored

--- Core/network/network_lib/src/main/java/co/in/nnj/learn/net/ex5/SimpleUDPServer.java
+++ Core/network/network_lib/src/main/java/co/in/nnj/learn/net/ex5/SimpleUDPServer.java
@@ -1,40 +1,12 @@
 package co.in.nnj.learn.net.ex5;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.util.ArrayList;
-import java.util.List;
 
 public class SimpleUDPServer {
 
-    private static List<String> readQuates() {
-        final ArrayList<String> quates = new ArrayList<>();
-        final InputStream inputStream = SimpleUDPServer.class.getResourceAsStream("quotes.txt");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line = reader.readLine();
-            while (line != null) {
-                quates.add(line);
-                line = reader.readLine();
-            }
-        } catch (final IOException e) {
-        }
-        // -- Start udp server
-        System.out.println("Number of quates available - " + quates.size());
-        return quates;
-    }
-
-    static String getAnyQuate(final List<String> quates) {
-        final int index = (int) (Math.random() * quates.size());
-        if (index < quates.size()) {
-            return quates.get(index);
-        }
-        return "Patience is bitter, but its fruit is sweet. Try one more time.";
-    }
 
     private static void sendQuateToClient(final String quate, final DatagramPacket packet,
             final DatagramSocket server) throws IOException {
@@ -48,7 +20,7 @@ public class SimpleUDPServer {
 
     public static void main(final String[] args) {
         // -- Read file
-        final List<String> quates = readQuates();
+        Quates.init();
 
         try (DatagramSocket server = new DatagramSocket(5000)) {
             // -- Read request -
@@ -61,7 +33,7 @@ public class SimpleUDPServer {
                     System.out.println("request - " + req);
 
                     // -- Send respose as quate
-                    final String quate = getAnyQuate(quates);
+                    final String quate = Quates.getAnyQuate();
                     sendQuateToClient(quate, dgPacket, server);
                 } catch (final Exception e) {
                 }
--- Core/network/network_lib/src/main/java/co/in/nnj/learn/net/ex5/SimpleUDPServerWithSelector.java
+++ Core/network/network_lib/src/main/java/co/in/nnj/learn/net/ex5/SimpleUDPServerWithSelector.java
@@ -1,18 +1,13 @@
 package co.in.nnj.learn.net.ex5;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /*
@@ -20,32 +15,8 @@ import java.util.concurrent.TimeUnit;
  */
 public class SimpleUDPServerWithSelector {
 
-    private static List<String> readQuates() {
-        final ArrayList<String> quates = new ArrayList<>();
-        final InputStream inputStream = SimpleUDPServer.class.getResourceAsStream("quotes.txt");
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-            String line = reader.readLine();
-            while (line != null) {
-                quates.add(line);
-                line = reader.readLine();
-            }
-        } catch (final IOException e) {
-        }
-        // -- Start udp server
-        System.out.println("Number of quates available - " + quates.size());
-        return quates;
-    }
-
-    static String getAnyQuate(final List<String> quates) {
-        final int index = (int) (Math.random() * quates.size());
-        if (index < quates.size()) {
-            return quates.get(index);
-        }
-        return "Patience is bitter, but its fruit is sweet. Try one more time.";
-    }
-
     public static void main(final String[] args) {
-        final List<String> quates = readQuates();
+        Quates.init();
         try (DatagramChannel server = DatagramChannel.open()) {
             server.bind(new InetSocketAddress(5000));
             server.configureBlocking(false);
@@ -70,7 +41,7 @@ public class SimpleUDPServerWithSelector {
                         buffer.get(input);
                         System.out.println("Input => " + new String(input, 0, input.length));
 
-                        final byte[] anyQuate = getAnyQuate(quates).getBytes();
+                        final byte[] anyQuate = Quates.getAnyQuate().getBytes();
                         int offset = 0;
                         while (offset < anyQuate.length) {
                             buffer.clear();
