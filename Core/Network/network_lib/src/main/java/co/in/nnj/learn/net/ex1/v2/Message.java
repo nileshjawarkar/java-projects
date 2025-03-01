bc9a593889171669c3b01f24588651a8f96e5187
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-03-01 15:01:06 +0530

    Updates

--- Core/Network/network_lib/src/main/java/co/in/nnj/learn/net/ex1/v2/Message.java
+++ Core/Network/network_lib/src/main/java/co/in/nnj/learn/net/ex1/v2/Message.java
@@ -20,61 +20,6 @@ public class Message implements Serializable {
 
     @Override
     public String toString() {
-        return "Message{data=" + data + "}";
+        return "data [" + data + "]";
     }
-
-    /*
-     * private transient StringBuffer buf;
-     * public Message() {
-     * }
-     * 
-     * public Message(final String msg) {
-     * this.buf = new StringBuffer();
-     * append(msg);
-     * }
-     * 
-     * public StringBuffer getBuf() {
-     * return buf;
-     * }
-     * 
-     * public void setBuf(final StringBuffer buf) {
-     * this.buf = buf;
-     * }
-     * 
-     * public void append(final String msg) {
-     * buf.append(msg);
-     * buf.append(", ");
-     * }
-     * 
-     * private void writeObject(final ObjectOutputStream sout) throws Exception {
-     * final byte[] bytes = getBytes();
-     * sout.writeInt(bytes.length);
-     * sout.write(bytes);
-     * sout.flush();
-     * }
-     * 
-     * private void readObject(final ObjectInputStream sin) throws Exception {
-     * final int len = sin.readInt();
-     * if (len > 0) {
-     * final byte[] data = new byte[len];
-     * if (sin.read(data) > 0) {
-     * append(new String(data));
-     * }
-     * }
-     * }
-     * 
-     * public byte[] getBytes() {
-     * return buf.toString().getBytes();
-     * }
-     * 
-     * @Override
-     * public String toString() {
-     * return buf.toString();
-     * }
-     * 
-     * public int length() {
-     * return buf.length();
-     * }
-     * 
-     */
 }
