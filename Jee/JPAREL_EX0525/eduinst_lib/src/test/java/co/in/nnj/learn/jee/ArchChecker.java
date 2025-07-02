3a9d71ce4a2cf9e0a13be97d77aeb812c2dec4a6
nileshjawarkar <jawarkar.nilesh@gmail.com>
2025-07-02 19:58:40 +0530

    Added ArchTest

--- Jee/JPAREL_EX0525/eduinst_lib/src/test/java/co/in/nnj/learn/jee/ArchChecker.java
+++ Jee/JPAREL_EX0525/eduinst_lib/src/test/java/co/in/nnj/learn/jee/ArchChecker.java
@@ -1,5 +1,6 @@
 package co.in.nnj.learn.jee;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
@@ -10,22 +11,28 @@ import com.tngtech.archunit.lang.ArchRule;
 
 public class ArchChecker {
 
-    @Test
-    public void pkgAdapterShouldNotBeAccessedBy() {
-        final JavaClasses all_classes = new ClassFileImporter().importPackages("co.in.nnj.learn.jee");
-        final ArchRule ruleForAdapter1 = noClasses()
-                .that().resideInAPackage("..domain..")
-                .should().accessClassesThat().resideInAPackage("..adapter..");
-        ruleForAdapter1.check(all_classes);
+    private JavaClasses all_classes;
 
-        final ArchRule ruleForAdapter2 = noClasses()
-                .that().resideInAPackage("..port..")
-                .should().accessClassesThat().resideInAPackage("..adapter..");
-        ruleForAdapter2.check(all_classes);
+    @BeforeEach
+    public void init() {
+        all_classes = new ClassFileImporter().importPackages("co.in.nnj.learn.jee");
     }
 
     @Test
-    public void pkgInputOutputShouldNotAccessEachOther() {
+    public void pkgsShouldNotAccesseAdapter() {
+        final ArchRule ruleForDomain = noClasses()
+                .that().resideInAPackage("..domain..")
+                .should().accessClassesThat().resideInAPackage("..adapter..");
+        ruleForDomain.check(all_classes);
+
+        final ArchRule ruleForPort = noClasses()
+                .that().resideInAPackage("..port..")
+                .should().accessClassesThat().resideInAPackage("..adapter..");
+        ruleForPort.check(all_classes);
+    }
+
+    @Test
+    public void pkgsShouldNotAccessEachOther() {
         final JavaClasses adapter_classes = new ClassFileImporter().importPackages("co.in.nnj.learn.jee.adapter");
         final ArchRule ruleForInput = noClasses()
                 .that().resideInAPackage("..input..")
@@ -35,5 +42,34 @@ public class ArchChecker {
                 .that().resideInAPackage("..output..")
                 .should().accessClassesThat().resideInAPackage("..input..");
         ruleForOutput.check(adapter_classes);
+    }
+
+    @Test
+    public void pkgsShouldNotAccessPort() {
+        /*
+        final ArchRule ruleForEntity = noClasses()
+                .that().resideInAPackage("..domain.entity..")
+                .should().accessClassesThat().resideInAPackage("..port..");
+        ruleForEntity.check(all_classes); */
+
+        final ArchRule ruleForValueObjs = noClasses()
+                .that().resideInAPackage("..domain.valueobjects..")
+                .should().accessClassesThat().resideInAPackage("..port..");
+        ruleForValueObjs.check(all_classes);
+    }
+
+    @Test
+    public void pkgsShouldNotAccessServices() {
+        final JavaClasses domain_classes = new ClassFileImporter().importPackages("co.in.nnj.learn.jee.domain");
+        /*
+        final ArchRule ruleForEntity = noClasses()
+                .that().resideInAPackage("..entity..")
+                .should().accessClassesThat().resideInAPackage("..service..");
+        ruleForEntity.check(domain_classes); */
+
+        final ArchRule ruleForValueObjs = noClasses()
+                .that().resideInAPackage("..valueobjects..")
+                .should().accessClassesThat().resideInAPackage("..service..");
+        ruleForValueObjs.check(domain_classes);
     }
 }
